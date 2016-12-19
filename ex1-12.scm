485c3ad6996cc65334bf4fbf9f3d959a3a53a2e6
Kerry Buckley <kerryjbuckley@gmail.com>
2016-12-19 11:18:39 +0000

    Exercise 1.12

---
+++ ex1-12.scm
@@ -0,0 +1,15 @@
+(define (pascal-row n)
+  (define (next-row row)
+    (next-row~ row (list 1)))
+
+  (define (next-row~ remainder result)
+    (cond ((= (length remainder) 0) result)
+          ((= (length remainder) 1) (cons 1 result))
+          (else (next-row~ (cdr remainder)
+                           (cons (+ (car remainder)
+                                    (car (cdr remainder)))
+                                 result)))))
+
+  (if (= n 1)
+      (list 1)
+      (next-row (pascal-row (- n 1)))))
