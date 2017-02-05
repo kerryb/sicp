d6ac30db48117166e36764ae3d17131a5b601760
Kerry Buckley <kerryjbuckley@gmail.com>
2017-02-05 18:31:35 +0000

    Exercise 2.29a

---
+++ ex2-29.scm
@@ -0,0 +1,13 @@
+(define (make-mobile left right)
+  (list left right))
+(define (left-branch mobile)
+  (car mobile))
+(define (right-branch mobile)
+  (cadr mobile))
+
+(define (make-branch length structure)
+  (list length structure))
+(define (branch-length structure)
+  (car structure))
+(define (branch-structure structure)
+  (cadr structure))
