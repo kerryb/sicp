aa338032ecf1a75435c338ea3f8871f7f2c29b89
Kerry Buckley <kerryjbuckley@gmail.com>
2017-02-05 18:47:38 +0000

    Exercise 2.29b

--- ex2-29.scm
+++ ex2-29.scm
@@ -11,3 +11,27 @@
   (car structure))
 (define (branch-structure structure)
   (cadr structure))
+
+(define (total-weight mobile)
+  (define (branch-weight branch)
+    (if (pair? (branch-structure branch))
+      (total-weight (branch-structure branch))
+      (branch-structure branch)))
+  (+ (branch-weight (left-branch mobile))
+     (branch-weight (right-branch mobile))))
+
+; Test/demo thing … I should really look into scheme unit testing.
+
+(define l2 (make-branch 3 1))
+(define r2 (make-branch 1 3))
+(define m2 (make-mobile l2 r2))
+(define l (make-branch 4 2))
+(define r (make-branch 2 m2))
+(define m (make-mobile l r))
+
+(newline)
+(display "Weight of right submobile: ")
+(display (total-weight m2))
+(newline)
+(display "Total weight: ")
+(display (total-weight m))
