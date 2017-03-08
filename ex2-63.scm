35a23216b5037b8ab29ed98b202fbe7955052a6d
Kerry Buckley <kerryjbuckley@gmail.com>
2017-03-08 21:09:23 +0000

    Exercise 2.63 (kind of)

---
+++ ex2-63.scm
@@ -0,0 +1,55 @@
+(define (entry tree) (car tree))
+(define (left-branch tree) (cadr tree))
+(define (right-branch tree) (caddr tree))
+(define (make-tree entry left right)
+  (list entry left right))
+
+(define tree-1
+  (make-tree 7
+             (make-tree 3
+                        (make-tree 1 '() '())
+                        (make-tree 5 '() '()))
+             (make-tree 9
+                        '()
+                        (make-tree 11 '() '()))))
+
+(define tree-2
+  (make-tree 3
+             (make-tree 1 '() '())
+             (make-tree 7
+                        (make-tree 5 '() '())
+                        (make-tree 9
+                                   '()
+                                   (make-tree 11 '() '())))))
+
+(define tree-3
+  (make-tree 5
+             (make-tree 3
+                        (make-tree 1 '() '())
+                        '())
+             (make-tree 9
+                        (make-tree 7 '() '())
+                        (make-tree 11 '() '()))))
+
+(define (tree->list-1 tree)
+  (if (null? tree)
+    '()
+    (append (tree->list-1 (left-branch tree))
+            (cons (entry tree)
+                  (tree->list-1
+                    (right-branch tree))))))
+
+(define (tree->list-2 tree)
+  (define (copy-to-list tree result-list)
+    (if (null? tree)
+      result-list
+      (copy-to-list (left-branch tree)
+                    (cons (entry tree)
+                          (copy-to-list
+                            (right-branch tree)
+                            result-list)))))
+  (copy-to-list tree '()))
+
+; (a) Both return the same lists: (1 3 5 7 9 11)
+; (b) the second one probably grows more slowly, as it's slowly building up one
+; list, rather than concatenating lots of little ones.
